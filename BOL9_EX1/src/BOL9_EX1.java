22c5df606f67c082cffdc00605a7ff83896cbcb5
sergio1011 <agommej008@gmail.com>
2020-01-16 11:23:10 +0100

    Ya acabé. closed #5

--- BOL9_EX1/src/BOL9_EX1.java
+++ BOL9_EX1/src/BOL9_EX1.java
@@ -14,6 +14,6 @@ public class BOL9_EX1 {
 				"Actualmente el stock del producto -" + p.getNombre() + "- es de " + p.getUnidades() + " unidades");
 		System.out.println(
 				"El valor del stock del producto -" + p.getNombre() + "- es de " + p.valorEnStock() + " euros");
-//para ustedes gg
+//para ustedes gg hh
 	}
 }
